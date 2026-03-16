8d3dea358425440562525d191ea2131dee644cf7
walt west <waltwest@outlook.com>
2026-03-16 17:39:40 -0400

    adding foods

--- foods.py
+++ foods.py
@@ -2,26 +2,27 @@
 # (cals, protein) per unit
 whole_foods = {
     'egg': (77,6),
-    'banana': (105, None)
+    'banana': (105, 0)
 }
 
 # structured as (cals/gram, protein/gram)
 weighted_foods = {
-    "raw chicken": (1.65, 0.31),
-    "canned chicken": (0.83, 0.18),
-    "raw beef": (1.88, 0.21),
-    "carved turkey": (1.07, 0.21),
-    "veggie medley": (0.35, 0.02),
-    "cucumber": (0.15, None),
-    "tomato": (0.20, None),
-    "green grapes": (0.70, None),
-    "ground turkey": (1.42, 0.20),
-    "sliced ham": (1.07, 0.16),
-    "flounder filets": (0.8, 0.17),
-    "blueberries": (0.57, None),
-    "zucchini": (0.17, None),
-    "brussel sprouts": (0.45, None),
-    "brown rice": (1.1, 0.02),
-    "raspberry": (0.52, None),
-    "broccoli": (0.34, None)
+    'chicken breast': (1.65, 0.31),
+    'canned chicken': (0.83, 0.18),
+    'beef': (1.88, 0.21),
+    'carved turkey': (1.07, 0.21),
+    'veggie medley': (0.35, 0.02),
+    'cucumber': (0.15, 0),
+    'tomato': (0.20, 0),
+    'green grapes': (0.70, 0),
+    'ground turkey': (1.42, 0.20),
+    'sliced ham': (1.07, 0.16),
+    'flounder filets': (0.8, 0.17),
+    'blueberries': (0.57, 0),
+    'zucchini': (0.17, 0),
+    'brussels sprout': (0.45, 0),
+    'brown rice': (1.1, 0.02),
+    'raspberry': (0.52, 0),
+    'broccoli': (0.34, 0),
+    'sweet potato': (0.86, 0.016)
 }
